90526147cdeb3c2a05fdf4129f61b395f911d7b1
Dima Brusilovsky <dimabru@gmail.com>
2022-08-25 19:31:48 +0300

    build: release to brew (#5)
    * wip: test release brew
    
    * wip: shell bash
    
    * fix: name
    
    * wip: chmod brew scripts
    
    * run bash
    
    * set -e
    
    * set git config
    
    * setup github token
    
    * ci github token secret
    
    * build: push to brew

---
+++ scripts/brew_push_formula.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+if [ $# -lt 1 ]
+  then
+    echo "Missing version"
+    exit
+fi
+
+VERSION=$1
+
+BREW_REPO_URL="https://${GITHUB_TOKEN}@github.com/allero-io/homebrew-allero.git"
+
+git config --global user.email "ci@allero.io"
+git config --global user.name "Allero CI"
+
+git clone $BREW_REPO_URL
+bash ./scripts/brew_formula_generator.sh $VERSION
+cd homebrew-allero
+git add -A
+git commit -m "Brew formula update for allero version $VERSION"
+git push
+cd ..
