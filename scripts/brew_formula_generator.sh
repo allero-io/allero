e98b3a3c2ea18b44aec15a6093505ecbafcf3875
Dima Brusilovsky <dimabru@gmail.com>
2022-08-25 18:52:08 +0300

    wip: test release brew

---
+++ scripts/brew_formula_generator.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+  then
+    echo "Missing version"
+    exit
+fi
+
+VERSION=$1
+
+SHA256_MAC_INTEL=$(cat ./dist/checksums.txt | grep Darwin_x86_64 | cut -d" " -f1)
+SHA256_MAC_ARM=$(cat ./dist/checksums.txt | grep Darwin_arm64 | cut -d" " -f1)
+SHA256_LINUX_INTEL=$(cat ./dist/checksums.txt | grep Linux_x86_64 | cut -d" " -f1)
+SHA256_LINUX_ARM=$(cat ./dist/checksums.txt | grep Linux_arm64 | cut -d" " -f1)
+
+cat > homebrew-allero/allero.rb <<-EOF
+# typed: false
+# frozen_string_literal: true
+class Allero < Formula
+  desc ""
+  homepage "https://allero.io/"
+  version "$VERSION"
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/allero-io/allero/releases/download/$VERSION/allero_${VERSION}_Darwin_x86_64.zip"
+    sha256 "$SHA256_MAC_INTEL"
+  end
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/allero-io/allero/releases/download/$VERSION/allero_${VERSION}_Darwin_arm64.zip"
+    sha256 "$SHA256_MAC_ARM"
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/allero-io/allero/releases/download/$VERSION/allero_${VERSION}_Linux_x86_64.zip"
+    sha256 "$SHA256_LINUX_INTEL"
+  end
+  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://github.com/allero-io/allero/releases/download/$VERSION/allero_${VERSION}_Linux_arm64.zip"
+    sha256 "$SHA256_LINUX_ARM"
+  end
+  def install
+    bin.install "allero"
+  end
+  def caveats
+    <<~EOS
+      \033[32m[V] Downloaded Datree
+      [V] Finished Installation
+      \033[35m Usage: $ allero fetch github <owner|owner/repo ...>
+    EOS
+  end
+end
+EOF
---
+++ scripts/brew_push_formula.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+  then
+    echo "Missing version"
+    exit
+fi
+
+VERSION=$1
+
+BREW_REPO_URL="https://${GITHUB_TOKEN}@github.com/allero-io/homebrew-allero.git"
+
+git clone $BREW_REPO_URL
+bash ./scripts/brew_formula_generator.sh $VERSION
+cd homebrew-allero
+git add -A
+git commit -m "Brew formula update for allero version $VERSION"
+git push
+cd ..
