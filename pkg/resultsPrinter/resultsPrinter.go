baf4ee94b73418f877e922c9ca7bbfd9a0dbcd80
Dima Brusilovsky <dimabru@gmail.com>
2022-09-22 12:53:55 +0300

    feat: gitlab validate support

--- pkg/resultsPrinter/resultsPrinter.go
+++ pkg/resultsPrinter/resultsPrinter.go
@@ -3,6 +3,7 @@ package resultsPrinter
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 
 	"github.com/allero-io/allero/pkg/rulesConfig"
@@ -15,11 +16,16 @@ var CICD_PLATFORMS = map[string]string{
 	"jfrog-pipelines":          "Jfrog Pipelines",
 }
 
+var SCM_PLATFORMS = map[string]string{
+	"github": "Github",
+	"gitlab": "Gitlab",
+}
+
 const colorRed = "\033[31m"
 const colorReset = "\033[0m"
 const colorBlue = "\033[34m"
 
-func PrintResults(ruleResults []*rulesConfig.RuleResult, summary rulesConfig.OutputSummary, outputFormat string) error {
+func PrintResults(ruleResults map[int]*rulesConfig.RuleResult, summary rulesConfig.OutputSummary, outputFormat string) error {
 	if outputFormat == "" {
 		printPretty(ruleResults, summary)
 		printSummary(ruleResults, summary)
@@ -30,12 +36,15 @@ func PrintResults(ruleResults []*rulesConfig.RuleResult, summary rulesConfig.Out
 	return nil
 }
 
-func printPretty(ruleResults []*rulesConfig.RuleResult, summary rulesConfig.OutputSummary) {
+func printPretty(ruleResults map[int]*rulesConfig.RuleResult, summary rulesConfig.OutputSummary) {
+	ruleIds := sortRulesOrder(ruleResults)
+
 	headingColored := color.New(color.FgCyan, color.Bold, color.Underline)
 	headingColored.Println("Allero Pipelines Validation Results")
 	fmt.Println()
 
-	for _, ruleResult := range ruleResults {
+	for _, id := range ruleIds {
+		ruleResult := ruleResults[id]
 		fmt.Println("Rule:", ruleResult.RuleName)
 
 		if ruleResult.Valid {
@@ -52,7 +61,7 @@ func printPretty(ruleResults []*rulesConfig.RuleResult, summary rulesConfig.Outp
 				uneascapedRepoName := unescapeValue(schemaError.RepositryName)
 				uneascapedFilepath := unescapeValue(schemaError.WorkflowRelPath)
 
-				t.AppendRow([]interface{}{"Github", CICD_PLATFORMS[schemaError.CiCdPlatform], schemaError.OwnerName, uneascapedRepoName, uneascapedFilepath})
+				t.AppendRow([]interface{}{SCM_PLATFORMS[schemaError.ScmPlatform], CICD_PLATFORMS[schemaError.CiCdPlatform], schemaError.OwnerName, uneascapedRepoName, uneascapedFilepath})
 				t.AppendSeparator()
 			}
 			t.Render()
@@ -63,11 +72,21 @@ func printPretty(ruleResults []*rulesConfig.RuleResult, summary rulesConfig.Outp
 	}
 }
 
+func sortRulesOrder(ruleResults map[int]*rulesConfig.RuleResult) []int {
+	var keys []int
+	for k := range ruleResults {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+
+	return keys
+}
+
 func unescapeValue(value string) string {
 	return strings.ReplaceAll(value, "[ESCAPED_DOT]", ".")
 }
 
-func printSummary(ruleResults []*rulesConfig.RuleResult, summary rulesConfig.OutputSummary) {
+func printSummary(ruleResults map[int]*rulesConfig.RuleResult, summary rulesConfig.OutputSummary) {
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
 	fmt.Println("Summary")
