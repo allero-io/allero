5414640f294d345b9a75d346b838d30be7198538
OriYosef <45240478+OriYosef@users.noreply.github.com>
2022-09-28 12:55:35 +0300

    feat: order failed rules output

--- pkg/resultsPrinter/resultsPrinter.go
+++ pkg/resultsPrinter/resultsPrinter.go
@@ -111,7 +111,9 @@ func printSummary(ruleResults map[int]*rulesConfig.RuleResult, summary rulesConf
 	if summary.TotalFailedRules > 0 {
 		fmt.Println()
 		fmt.Println("Failed rules summary:")
-		for _, ruleResult := range ruleResults {
+		ruleIds := sortRulesOrder(ruleResults)
+		for _, id := range ruleIds {
+			ruleResult := ruleResults[id]
 			if !ruleResult.Valid {
 				fmt.Println(string(colorRed), "\r", ruleResult.RuleName, string(colorReset))
 			}
