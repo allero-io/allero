c055c5c47553def123f124e2037345f62ceab987
Martin Juarez <juarezmachadojorgemartin@gmail.com>
2022-10-08 18:16:18 -0400

    Improvements: A new function was added to the file gitlabClient.go; such function will generate an appropriate gitlab client based on the configurations provided by the user.

--- pkg/clients/gitlabClient.go
+++ pkg/clients/gitlabClient.go
@@ -1,16 +1,41 @@
 package clients
 
 import (
+	"fmt"
 	"github.com/allero-io/allero/pkg/configurationManager"
 	"github.com/xanzy/go-gitlab"
 )
 
 func CreateGitlabClient(configurationManager configurationManager.ConfigurationManager) (*gitlab.Client, error) {
 	GITLAB_TOKEN := configurationManager.GetGitlabToken()
-	gitlabClient, err := gitlab.NewClient(GITLAB_TOKEN)
+
+	gitlabCustomServerValue, err := configurationManager.Get("")
 	if err != nil {
 		return nil, err
 	}
 
+	gitlabPrivateServerURL := fmt.Sprintf("%s", gitlabCustomServerValue)
+
+	gitlabClient, err := generateGitlabClient(GITLAB_TOKEN, gitlabPrivateServerURL) 
+	if err != nil {
+		return nil, err
+	}
+
+	return gitlabClient, nil
+}
+
+func generateGitlabClient(gitlabToken, gitlabPrivateServerUrl string) (*gitlab.Client, error) {
+	if gitlabPrivateServerUrl != "" {
+		gitlabClient, err := gitlab.NewClient(gitlabToken, gitlab.WithBaseURL(gitlabPrivateServerUrl))
+		if err != nil {
+			return nil, err
+		}
+		return gitlabClient, nil
+	}
+
+	gitlabClient, err := gitlab.NewClient(gitlabToken)
+	if err != nil {
+		return nil, err
+	}
 	return gitlabClient, nil
 }
